09709d804633eb6f7797462ef37e86a129edbbf7
mar3a-sch <maria.schulz@student.htw-berlin.de>
2025-11-07 10:39:34 +0100

    Milestone 2

--- Feelique/src/router/index.ts
+++ Feelique/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MoodComponent from "@/components/MoodComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,8 +25,11 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/MoodTracker.vue'),
+      component: () => import('../components/MoodComponent.vue')
     },
+
+
+
 
   ],
 })
